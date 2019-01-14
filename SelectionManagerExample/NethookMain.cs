b5d138b0ea0791d0bfd0cf2d9f71cb61f0c62455
CraftyShane <43580125+CraftyShane@users.noreply.github.com>
2019-01-14 09:53:14 -0500

    Add project files.

---
+++ SelectionManagerExample/NethookMain.cs
@@ -0,0 +1,113 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NethookMain.cs" company="CNC Software, Inc.">
+//   Copyright (c) 2013 CNC Software, Inc.
+// </copyright>
+// <summary>
+//   Describes this class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SelectionManagerExample
+{
+    using System.Linq;
+
+    using Mastercam.App;
+    using Mastercam.App.Types;
+    using Mastercam.IO;
+    using Mastercam.IO.Types;
+    using Mastercam.Support;
+
+    using Resources;
+    using Services;
+
+    /// <summary>
+    /// Describes this class.
+    /// </summary>
+    public class NethookMain : NetHook3App
+    {
+        #region Public Override Methods
+
+        /// <summary>
+        /// The main entry point for your NETHook.
+        /// </summary>
+        /// <param name="param">System parameter.</param>
+        /// <returns>A <c>MCamReturn</c> return type representing the outcome of your NetHook application.</returns>
+        public override MCamReturn Run(int param)
+        {
+            if (UtilityService.Initialize())
+            {
+                // AskForPointGeometry
+                var point = UtilityService.AskForPointGeometry();
+                DialogManager.OK(
+                    point != null ? ApplicationStrings.SelectedPoint : ApplicationStrings.NothingSelected,
+                    ApplicationStrings.SelectAPoint);
+
+                this.Reset();
+
+                // AskForArcGeometry
+                var arc = UtilityService.AskForArcGeometry();
+                DialogManager.OK(
+                    arc != null ? ApplicationStrings.SelectedArc : ApplicationStrings.NothingSelected,
+                    ApplicationStrings.SelectAnArc);
+
+                this.Reset();
+
+                // AskForLineGeometry
+                var line = UtilityService.AskForLineGeometry();
+                DialogManager.OK(
+                    line != null ? ApplicationStrings.SelectedLine : ApplicationStrings.NothingSelected,
+                    ApplicationStrings.SelectALine);
+
+                this.Reset();
+
+                // AskForGeometry
+                var geometryType = UtilityService.AskForGeometry();
+                DialogManager.OK(geometryType, ApplicationStrings.AskForGeometry);
+
+                this.Reset();
+
+                // AskForMultipleGeometry
+                var geometries = UtilityService.AskForMultipleGeometry();
+                DialogManager.OK(
+                    geometries.Any() ? ApplicationStrings.SelectionSuccessful : ApplicationStrings.NothingSelected,
+                    ApplicationStrings.SelectMultipleGeometry);
+
+                this.Reset();
+
+                // BoxSelectGeometry Inside
+                if (!UtilityService.BoxSelectGeometry(BoxSelectionType.Inside))
+                {
+                    DialogManager.OK(ApplicationStrings.NothingSelected, ApplicationStrings.BoxSelectionInside);
+                }
+                else
+                {
+                    var selectionCount = SearchManager.GetSelectedGeometry().Count();
+                    DialogManager.OK(ApplicationStrings.BoxSelectionSuccessful + "\n" + ApplicationStrings.CountOfSelection + ": " + selectionCount, ApplicationStrings.BoxSelectionInside);
+                }
+
+                DialogManager.OK(ApplicationStrings.ExampleComplete, string.Empty);
+            }
+            else
+            {
+                DialogManager.Error(ApplicationStrings.MissingDrawing, ApplicationStrings.AskForGeometry);
+            }
+
+            return MCamReturn.NoErrors;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary> Resets the current selection if there is any. </summary>
+        private void Reset()
+        {
+            if (SearchManager.GetSelectedGeometry().Any())
+            {
+                SelectionManager.UnselectAllGeometry();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ SelectionManagerExample/Services/UtilityService.cs
@@ -0,0 +1,156 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UtilityService.cs" company="CNC Software, Inc.">
+//   Copyright (c) 2013 CNC Software, Inc.
+// </copyright>
+// <summary>
+//   The utility service.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SelectionManagerExample.Services
+{
+    using System.Collections.Generic;
+    using System.IO;
+
+    using Mastercam.BasicGeometry;
+    using Mastercam.Curves;
+    using Mastercam.Database;
+    using Mastercam.Database.Types;
+    using Mastercam.IO;
+    using Mastercam.IO.Types;
+    using Mastercam.Support;
+    using Resources;
+
+    /// <summary>
+    /// The utility service.
+    /// </summary>
+    public static class UtilityService
+    {
+        /// <summary>
+        /// The mold base file path.
+        /// </summary>
+        private static readonly string MoldBase = Path.Combine(SettingsManager.UserDirectory, @"mcx\Moldbase.mcx-8");
+
+        #region Internal Methods
+
+        /// <summary> Opens our test drawing </summary>
+        ///
+        /// <returns> true if it succeeds, false if it fails. </returns>
+        internal static bool Initialize()
+        {
+            const int ViewNumber = (int)GraphicsViewType.Iso;
+
+            // Start fresh
+            FileManager.New(false);
+
+            if (!File.Exists(MoldBase))
+            {
+                var msg = $"Missing File {MoldBase}. Please copy the included file to the applicable folder.";
+                DialogManager.OK(msg, "Missing File");
+                return false;
+            }
+
+            if (!FileManager.Open(MoldBase))
+            {
+                return false;
+            }
+
+            ViewManager.GraphicsView = SearchManager.GetViews(ViewNumber)[0];
+            GraphicsManager.FitScreen();
+            GraphicsManager.Repaint(true);
+            SelectionManager.UnselectAllGeometry();
+            GraphicsManager.ClearColors(new GroupSelectionMask());
+
+            return true;
+        }
+
+        /// <summary> Box select geometry. </summary>
+        ///
+        /// <param name="selectionType"> Type of the selection. </param>
+        ///
+        /// <returns> true if it succeeds, false if it fails. </returns>
+        internal static bool BoxSelectGeometry(BoxSelectionType selectionType)
+        {
+            return SelectionManager.BoxSelectGeometry(ApplicationStrings.BoxSelection, new GeometryMask(true), selectionType);
+        }
+
+        /// <summary> Ask for multiple geometry. </summary>
+        ///
+        /// <returns> A list of selected geometries. </returns>
+        internal static IEnumerable<Geometry> AskForMultipleGeometry()
+        {
+            return SelectionManager.AskForMultipleGeometry(ApplicationStrings.SelectMultipleGeometry, new GeometryMask(true));
+        }
+
+        /// <summary> Executes the ask for arc geometry action. </summary>
+        ///
+        /// <returns> An Arc Geometry. </returns>
+        internal static ArcGeometry AskForArcGeometry()
+        {
+            // Cast the return Geometry type to an explicit ArcGeometry, this is fine because we are masking on an Arc only
+            return AskUser(ApplicationStrings.SelectAnArc, new GeometryMask(false, false, true, false, false, false, false)) as ArcGeometry;
+        }
+
+        /// <summary> Executes the ask for line geometry action. </summary>
+        ///
+        /// <returns> A Line Geometry. </returns>
+        internal static LineGeometry AskForLineGeometry()
+        {
+            // Cast the return Geometry type to  an explicit LineGeometry, this is fine because we are masking on Line only
+            return AskUser(ApplicationStrings.SelectALine, new GeometryMask(false, true, false, false, false, false, false)) as LineGeometry;
+        }
+
+        /// <summary> Executes the ask for point geometry action. </summary>
+        ///
+        /// <returns> A PointGeometry. </returns>
+        internal static PointGeometry AskForPointGeometry()
+        {
+            // Cast the return Geometry type to an explicit PointGeometry, this is fine because we are masking on Points only
+            return AskUser(ApplicationStrings.SelectAPoint, new GeometryMask(true, false, false, false, false, false, false)) as PointGeometry;
+        }
+
+        /// <summary> Executes the ask for geometry action. </summary>
+        ///
+        /// <returns> A string representing the geometry type. </returns>
+        internal static string AskForGeometry()
+        {
+            // Mask on all geometry types
+            var geometry = AskUser(ApplicationStrings.SelectAnyGeometry, new GeometryMask(true));
+            if (geometry == null)
+            {
+                return ApplicationStrings.NothingSelected;
+            }
+
+            if (geometry.GetType() == typeof(ArcGeometry))
+            {
+                return ApplicationStrings.SelectedArc;
+            }
+
+            if (geometry.GetType() == typeof(LineGeometry))
+            {
+                return ApplicationStrings.SelectedLine;
+            }
+
+            return geometry.GetType() == typeof(PointGeometry)
+                       ? ApplicationStrings.SelectedPoint
+                       : ApplicationStrings.UnknownGeometry;
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        /// <summary> Initialises this object. </summary>
+        ///
+        /// <param name="prompt"> The prompt. </param>
+        /// <param name="mask">   The mask. </param>
+        ///
+        /// <returns> A Geometry. </returns>
+        private static Geometry AskUser(string prompt, GeometryMask mask)
+        {
+            return SelectionManager.AskForGeometry(prompt, mask);
+        }
+
+        #endregion
+    }
+}
